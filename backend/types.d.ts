9df7fc65c3d18d35f3d8d40b8801557cfbf8024d
zanylbajguttieva <janabaiguttieva@gmail.com>
2024-03-27 16:03:53 +0600

    Added User.ts and types.d.ts

---
+++ backend/types.d.ts
@@ -0,0 +1,18 @@
+import {Model} from "mongoose";
+
+export interface UserFields {
+    email: string;
+    password: string;
+    token: string;
+    role: string;
+    displayName: string;
+    googleId?: string;
+    avatar: string | null;
+}
+
+interface UserMethods {
+    checkPassword(password: string): Promise<boolean>;
+    generateToken(): void;
+}
+
+type UserModel = Model<UserFields, unknown, UserMethods>;
